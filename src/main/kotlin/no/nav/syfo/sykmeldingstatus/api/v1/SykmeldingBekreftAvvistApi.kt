096f765c63fd6880085ad8638c1c1f77926f77cd
Sebastian <sebastian.knudsen@nav.no>
2021-05-10 15:47:43 +0200

    Api for bekreftAvvist (#59)
    * Api for bekreftAvvist
    
    * New counter for bekreftet avvist
    
    * register endpoint

---
+++ src/main/kotlin/no/nav/syfo/sykmeldingstatus/api/v1/SykmeldingBekreftAvvistApi.kt
@@ -0,0 +1,30 @@
+package no.nav.syfo.sykmeldingstatus.api.v1
+
+import io.ktor.application.call
+import io.ktor.auth.authentication
+import io.ktor.auth.jwt.JWTPrincipal
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.Route
+import io.ktor.routing.post
+import no.nav.syfo.metrics.BEKREFTET_AVVIST_AV_BRUKER_COUNTER
+import no.nav.syfo.sykmeldingstatus.SykmeldingStatusService
+
+fun Route.registerSykmeldingBekreftAvvistApi(sykmeldingStatusService: SykmeldingStatusService) {
+    post("/api/v1/sykmeldinger/{sykmeldingid}/bekreftAvvist") {
+        val sykmeldingId = call.parameters["sykmeldingid"]!!
+        val token = call.request.headers["Authorization"]!!
+        val principal: JWTPrincipal = call.authentication.principal()!!
+        val fnr = principal.payload.subject
+
+        sykmeldingStatusService.registrerBekreftetAvvist(
+                sykmeldingId = sykmeldingId,
+                source = "user",
+                fnr = fnr,
+                token = token
+        )
+
+        BEKREFTET_AVVIST_AV_BRUKER_COUNTER.inc()
+        call.respond(HttpStatusCode.Accepted)
+    }
+}
