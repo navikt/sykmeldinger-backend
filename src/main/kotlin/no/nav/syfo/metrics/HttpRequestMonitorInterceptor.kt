b4c9d17f8785980a8f04c3d03bc2e7921ad62a67
Tia Firing <tia.firing@nav.no>
2022-06-02 07:29:38 +0200

    fjerner id-er fra metrikker

--- src/main/kotlin/no/nav/syfo/metrics/HttpRequestMonitorInterceptor.kt
+++ src/main/kotlin/no/nav/syfo/metrics/HttpRequestMonitorInterceptor.kt
@@ -6,12 +6,22 @@ import io.ktor.util.pipeline.PipelineContext
 
 val REGEX = """[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}""".toRegex()
 
+val REGEX_MOTTAKID = """[0-9]{10}[a-z]{4}[0-9]{5}.1""".toRegex()
+
+val REGEX_GAMMEL_ID = """ID:[0-9a-z]{48}""".toRegex()
+
 fun monitorHttpRequests(): suspend PipelineContext<Unit, ApplicationCall>.(Unit) -> Unit {
     return {
         val path = context.request.path()
-        val label = REGEX.replace(path, ":id")
+        val label = getLabel(path)
         val timer = HTTP_HISTOGRAM.labels(label).startTimer()
         proceed()
         timer.observeDuration()
     }
+}
+
+fun getLabel(path: String): String {
+    val utenId = REGEX.replace(path, ":id")
+    val utenMottakId = REGEX_MOTTAKID.replace(utenId, ":mottakId")
+    return REGEX_GAMMEL_ID.replace(utenMottakId, ":gammelId")
 }
