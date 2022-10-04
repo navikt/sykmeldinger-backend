367ab62a436f5cb02f87780a5e33c5c8bf5ec3d2
Tia Firing <tia.firing@nav.no>
2022-10-04 07:54:15 +0200

    fjerner nl-info som frontend ikke trenger

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -10,7 +10,6 @@ import io.prometheus.client.hotspot.DefaultExports
 import no.nav.syfo.application.ApplicationServer
 import no.nav.syfo.application.ApplicationState
 import no.nav.syfo.application.createApplicationEngine
-import no.nav.syfo.application.database.Database
 import no.nav.syfo.application.getWellKnown
 import no.nav.syfo.application.getWellKnownTokenX
 import no.nav.syfo.sykmeldingstatus.kafka.KafkaFactory.Companion.getSykmeldingStatusKafkaProducer
@@ -47,7 +46,7 @@ fun main() {
         .build()
 
     val applicationState = ApplicationState()
-    //val database = Database(env)
+    // val database = Database(env)
 
     DefaultExports.initialize()
 
