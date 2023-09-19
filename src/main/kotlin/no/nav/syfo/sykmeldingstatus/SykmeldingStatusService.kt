12a4d88b1b63aa14f41d77fa49d4ccf071c7cfef
Helene Arnesen <helene.arnesen@nav.no>
2023-09-19 12:20:20 +0200

    endrer fra securelogs til logs

--- src/main/kotlin/no/nav/syfo/sykmeldingstatus/SykmeldingStatusService.kt
+++ src/main/kotlin/no/nav/syfo/sykmeldingstatus/SykmeldingStatusService.kt
@@ -11,7 +11,6 @@ import no.nav.syfo.model.sykmeldingstatus.SporsmalOgSvarDTO
 import no.nav.syfo.model.sykmeldingstatus.SvartypeDTO
 import no.nav.syfo.model.sykmeldingstatus.SykmeldingStatusKafkaEventDTO
 import no.nav.syfo.objectMapper
-import no.nav.syfo.securelog
 import no.nav.syfo.sykmelding.SykmeldingService
 import no.nav.syfo.sykmelding.model.SykmeldingDTO
 import no.nav.syfo.sykmelding.model.SykmeldingsperiodeDTO
@@ -156,7 +155,7 @@ class SykmeldingStatusService(
 
             // kun for testing i dev
             if (tidligereArbeidsgiver != null) {
-                securelog.info("legger til tidligere arbeidsgiver for fnr: $fnr orgnummer: ${sykmeldingStatusKafkaEventDTO.tidligereArbeidsgiver?.orgnummer} ")
+                log.info("legger til tidligere arbeidsgiver for fnr: $fnr orgnummer: ${sykmeldingStatusKafkaEventDTO.tidligereArbeidsgiver?.orgnummer} ")
             }
 
             sykmeldingStatusKafkaProducer.send(
