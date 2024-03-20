e7dda0cc04dba7660b0d287122022d7e6c7eaee3
Helene Arnesen <helene.arnesen@nav.no>
2024-03-20 09:33:38 +0100

    feat: TidligereAg is only null when sykmeldt has one ag

--- src/test/kotlin/no/nav/syfo/sykmeldingstatus/SykmeldingStatusServiceSpec.kt
+++ src/test/kotlin/no/nav/syfo/sykmeldingstatus/SykmeldingStatusServiceSpec.kt
@@ -8,6 +8,10 @@ import io.mockk.coVerify
 import io.mockk.just
 import io.mockk.mockkClass
 import io.mockk.slot
+import java.time.LocalDate
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import kotlin.test.assertFailsWith
 import kotlinx.coroutines.runBlocking
 import no.nav.syfo.arbeidsgivere.model.Arbeidsgiverinfo
 import no.nav.syfo.arbeidsgivere.service.ArbeidsgiverService
@@ -28,6 +32,7 @@ import no.nav.syfo.sykmeldingstatus.api.v2.SykmeldingFormResponse
 import no.nav.syfo.sykmeldingstatus.db.SykmeldingStatusDb
 import no.nav.syfo.sykmeldingstatus.exception.InvalidSykmeldingStatusException
 import no.nav.syfo.sykmeldingstatus.exception.SykmeldingStatusNotFoundException
+import no.nav.syfo.sykmeldingstatus.exception.UserInputFlereArbeidsgivereIsNullException
 import no.nav.syfo.sykmeldingstatus.kafka.model.ShortNameKafkaDTO
 import no.nav.syfo.sykmeldingstatus.kafka.model.SporsmalOgSvarKafkaDTO
 import no.nav.syfo.sykmeldingstatus.kafka.model.SvartypeKafkaDTO
@@ -39,10 +44,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
-import java.time.OffsetDateTime
-import java.time.ZoneOffset
-import kotlin.test.assertFailsWith
 
 class SykmeldingStatusServiceSpec {
     val sykmeldingId = "id"
@@ -1189,19 +1190,16 @@ class SykmeldingStatusServiceSpec {
                     timestamp = OffsetDateTime.now(ZoneOffset.UTC).minusHours(1),
                     erAvvist = true,
                 )
-            sykmeldingStatusService.createStatus(
-                opprettBekreftetSykmeldingUserEvent(arbeidssituasjon = ARBEIDSLEDIG),
-                sykmeldingId,
-                fnr,
-            )
-
-            coVerify(exactly = 1) {
-                sykmeldingStatusDb.insertStatus(
-                    match { it.tidligereArbeidsgiver == null },
-                    any(),
-                    any(),
-                )
-            }
+            val exception =
+                assertFailsWith<UserInputFlereArbeidsgivereIsNullException> {
+                    sykmeldingStatusService.createStatus(
+                        opprettBekreftetSykmeldingUserEvent(arbeidssituasjon = ARBEIDSLEDIG),
+                        sykmeldingId,
+                        fnr,
+                    )
+                }
+            exception.message shouldBeEqualTo
+                "TidligereArbeidsgivereBrukerInput felt er null i flere-relevante-arbeidsgivere-flyten. Dette skal ikke være mulig for sykmeldingId $sykmeldingId"
         }
 
         @Test
