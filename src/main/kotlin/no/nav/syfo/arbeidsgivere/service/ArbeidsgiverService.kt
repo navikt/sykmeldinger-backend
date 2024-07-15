8f42e9fa004d72a830d6b6466a0ef4654b5601d7
Helene Arnesen <helene.arnesen@nav.no>
2024-07-15 13:51:29 +0200

    wip: arbeidsgivereapi

--- src/main/kotlin/no/nav/syfo/arbeidsgivere/service/ArbeidsgiverService.kt
+++ src/main/kotlin/no/nav/syfo/arbeidsgivere/service/ArbeidsgiverService.kt
@@ -2,15 +2,44 @@ package no.nav.syfo.arbeidsgivere.service
 
 import java.time.LocalDate
 import no.nav.syfo.arbeidsgivere.db.ArbeidsforholdDb
+import no.nav.syfo.arbeidsgivere.model.Arbeidsforhold
 import no.nav.syfo.arbeidsgivere.model.Arbeidsgiverinfo
 import no.nav.syfo.arbeidsgivere.model.NarmesteLeder
 import no.nav.syfo.arbeidsgivere.narmesteleder.db.NarmestelederDb
 import no.nav.syfo.arbeidsgivere.narmesteleder.db.NarmestelederDbModel
+import no.nav.syfo.sykmelding.model.SykmeldingDTO
 
 class ArbeidsgiverService(
     private val narmestelederDb: NarmestelederDb,
     private val arbeidsforholdDb: ArbeidsforholdDb,
 ) {
+
+    suspend fun getArbeidsgivereWithinSykmeldingPeriode(
+        sykmelding: SykmeldingDTO,
+        fnr: String,
+        date: LocalDate = LocalDate.now()
+    ): List<Arbeidsgiverinfo> {
+        val arbeidsgivere = arbeidsforholdDb.getArbeidsforhold(fnr = fnr)
+        if (arbeidsgivere.isEmpty()) {
+            return emptyList()
+        }
+        val aktiveNarmesteledere = narmestelederDb.getNarmesteleder(fnr)
+        val arbeidsgivereWithinSykmeldingsperiode = filterArbeidsgivere(sykmelding, arbeidsgivere)
+        return arbeidsgivereWithinSykmeldingsperiode.map { arbeidsforhold ->
+            val narmesteLeder =
+                aktiveNarmesteledere.find { it.orgnummer == arbeidsforhold.orgnummer }
+            Arbeidsgiverinfo(
+                orgnummer = arbeidsforhold.orgnummer,
+                juridiskOrgnummer = arbeidsforhold.juridiskOrgnummer,
+                navn = arbeidsforhold.orgNavn,
+                aktivtArbeidsforhold =
+                    arbeidsforhold.tom == null ||
+                        !date.isAfter(arbeidsforhold.tom) && !date.isBefore(arbeidsforhold.fom),
+                naermesteLeder = narmesteLeder?.tilNarmesteLeder(arbeidsforhold.orgNavn),
+            )
+        }
+    }
+
     suspend fun getArbeidsgivere(
         fnr: String,
         date: LocalDate = LocalDate.now()
@@ -48,5 +77,35 @@ class ArbeidsgiverService(
             orgnummer = orgnummer,
             organisasjonsnavn = orgnavn,
         )
+    }
+
+    private fun filterArbeidsgivere(
+        sykmelding: SykmeldingDTO,
+        allArbeidsgivere: List<Arbeidsforhold>,
+    ): List<Arbeidsforhold> {
+        val (sykmeldingFom, sykmeldingTom) = getSykmeldingFomTom(sykmelding)
+        return allArbeidsgivere.filter {
+            isArbeidsforholdWithinSykmeldingPeriode(it, sykmeldingFom, sykmeldingTom)
+        }
+    }
+
+    private fun isArbeidsforholdWithinSykmeldingPeriode(
+        arbeidsforhold: Arbeidsforhold,
+        sykmeldingFom: LocalDate,
+        sykmeldingTom: LocalDate,
+    ): Boolean {
+        val checkSluttdato =
+            arbeidsforhold.tom == null ||
+                arbeidsforhold.fom.isAfter(sykmeldingFom) ||
+                arbeidsforhold.tom == sykmeldingFom
+        val checkStartdato =
+            arbeidsforhold.fom.isBefore(sykmeldingTom) || arbeidsforhold.fom == sykmeldingTom
+        return checkStartdato && checkSluttdato
+    }
+
+    private fun getSykmeldingFomTom(sykmelding: SykmeldingDTO): Pair<LocalDate, LocalDate> {
+        val fom = sykmelding.sykmeldingsperioder.minOf { it.fom }
+        val tom = sykmelding.sykmeldingsperioder.maxOf { it.tom }
+        return fom to tom
     }
 }
--- src/main/kotlin/no/nav/syfo/brukerinformasjon/api/BrukerinformasjonApi.kt
+++ src/main/kotlin/no/nav/syfo/brukerinformasjon/api/BrukerinformasjonApi.kt
@@ -8,10 +8,12 @@ import io.ktor.server.routing.get
 import no.nav.syfo.arbeidsgivere.model.Arbeidsgiverinfo
 import no.nav.syfo.arbeidsgivere.service.ArbeidsgiverService
 import no.nav.syfo.plugins.BrukerPrincipal
+import no.nav.syfo.sykmelding.SykmeldingService
 import org.koin.ktor.ext.inject
 
 fun Route.registrerBrukerinformasjonApi() {
     val arbeidsgiverService by inject<ArbeidsgiverService>()
+    val sykmeldingService by inject<SykmeldingService>()
 
     get("/brukerinformasjon") {
         val principal: BrukerPrincipal = call.authentication.principal()!!
@@ -24,6 +26,30 @@ fun Route.registrerBrukerinformasjonApi() {
                 arbeidsgivere = arbeidsgivere,
             ),
         )
+    }
+
+    get("/arbeidsgivere/{sykmeldingId}") {
+        val sykmeldingId = call.parameters["sykmeldingid"]!!
+        val principal: BrukerPrincipal = call.authentication.principal()!!
+        val fnr = principal.fnr
+
+        val sykmelding = sykmeldingService.getSykmelding(fnr = fnr, sykmeldingid = sykmeldingId)
+        if (sykmelding != null) {
+            val arbeidsgivere =
+                arbeidsgiverService.getArbeidsgivereWithinSykmeldingPeriode(sykmelding, fnr = fnr)
+            call.respond(
+                Brukerinformasjon(
+                    arbeidsgivere = arbeidsgivere,
+                ),
+            )
+        } else {
+            val arbeidsgivere = arbeidsgiverService.getArbeidsgivere(fnr = fnr)
+            call.respond(
+                Brukerinformasjon(
+                    arbeidsgivere = arbeidsgivere,
+                ),
+            )
+        }
     }
 }
 
