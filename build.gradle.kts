6ba39d61ec2c274218de8151ac5aeff9209e79bc
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-10-15 19:52:00 +0200

    chore: bumped ktor to 3.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ group = "no.nav.syfo"
 version = "1.0.0"
 
 val coroutinesVersion = "1.9.0"
-val ktorVersion = "2.3.12"
+val ktorVersion = "3.0.0"
 val logbackVersion = "1.5.8"
 val logstashEncoderVersion = "8.0"
 val prometheusVersion = "0.16.0"
